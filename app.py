2b5c7968cb18571e5444d8583fe0f47253fc551a
Gurbaksh Kaur <gurbakshkaur127@gmail.com>
2026-04-21 20:11:56 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "<h1>Devops Successfull!</h1><p>Python+ Docker + Git + AWS running</p>"
+  return "<h1>Gurbaksh Kaur: Devops Successfull!</h1><p>Python+ Docker + Git + AWS running</p>"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
